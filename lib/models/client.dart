6e1abd339139e529e332a58edf8547938f755924
Dilhara Sannasgala <kavindu_2020691@fot.ruh.ac.lk>
2024-05-17 20:13:51 +0530

    Modified Invoice List

--- lib/models/client.dart
+++ lib/models/client.dart
@@ -1,52 +1,58 @@
 class Client {
   final int id;
-  final String name;
-  final String organizationName;
-  final String latitude;
-  final String longitude;
+  final String? name;
+  final String? organizationName;
+  final String? latitude;
+  final String? longitude;
   final String? phoneNo;
-  final String status;
-  final double discount;
-  final double creditLimit;
-  final int creditPeriod;
-  final int routeId;
-  final int addedByEmployeeId;
-  final DateTime createdAt;
-  final DateTime updatedAt;
+  final String? status;
+  final double? discount;
+  final double? creditLimit;
+  final int? creditPeriod;
+  final int? routeId;
+  final int? addedByEmployeeId;
+  final DateTime? createdAt;
+  final DateTime? updatedAt;
 
   Client({
     required this.id,
-    required this.name,
-    required this.organizationName,
-    required this.latitude,
-    required this.longitude,
+    this.name,
+    this.organizationName,
+    this.latitude,
+    this.longitude,
     this.phoneNo,
-    required this.status,
-    required this.discount,
-    required this.creditLimit,
-    required this.creditPeriod,
-    required this.routeId,
-    required this.addedByEmployeeId,
-    required this.createdAt,
-    required this.updatedAt,
+    this.status,
+    this.discount,
+    this.creditLimit,
+    this.creditPeriod,
+    this.routeId,
+    this.addedByEmployeeId,
+    this.createdAt,
+    this.updatedAt,
   });
 
   factory Client.fromJson(Map<String, dynamic> json) {
     return Client(
       id: json['id'],
-      name: json['name'],
-      organizationName: json['organization_name'],
-      latitude: json['latitude'],
-      longitude: json['longitude'],
-      phoneNo: json['phone_no'],
-      status: json['status'],
-      discount: double.parse(json['discount'].toString()),
-      creditLimit: double.parse(json['credit_limit'].toString()),
-      creditPeriod: json['credit_period'],
-      routeId: json['route_id'],
-      addedByEmployeeId: json['added_by_employee_id'],
-      createdAt: DateTime.parse(json['createdAt']),
-      updatedAt: DateTime.parse(json['updatedAt']),
+      name: json['name'] as String?,
+      organizationName: json['organization_name'] as String?,
+      latitude: json['latitude'] as String?,
+      longitude: json['longitude'] as String?,
+      phoneNo: json['phone_no'] as String?,
+      status: json['status'] as String?,
+      discount: json['discount'] != null
+          ? double.tryParse(json['discount'].toString())
+          : null,
+      creditLimit: json['credit_limit'] != null
+          ? double.tryParse(json['credit_limit'].toString())
+          : null,
+      creditPeriod: json['credit_period'] as int?,
+      routeId: json['route_id'] as int?,
+      addedByEmployeeId: json['added_by_employee_id'] as int?,
+      createdAt:
+          json['createdAt'] != null ? DateTime.parse(json['createdAt']) : null,
+      updatedAt:
+          json['updatedAt'] != null ? DateTime.parse(json['updatedAt']) : null,
     );
   }
 }
--- lib/services/invoice_api_service.dart
+++ lib/services/invoice_api_service.dart
@@ -6,56 +6,44 @@ import 'package:order_processing_app/utils/logger.dart';
 import 'package:logger/logger.dart';
 
 class InvoiceService {
-  
   static const String baseUrl = 'https://api.gsutil.xyz';
 
-  //get all invoices
+// get all invoices
   static Future<List<Invoice>> getInvoices() async {
     try {
       final response = await http.get(Uri.parse('$baseUrl/invoice'));
       if (response.statusCode == 200) {
         final List<dynamic> data = json.decode(response.body)['invoices'];
-        AppLogger.logInfo('Data received: $data'); // Add print statement here
-        return data
-            .map((json) => Invoice(
-                  id: json['id'],
-                  referenceNumber: json['reference_number'],
-                  clientId: json['client_id'],
-                  employeeId: json['employee_id'],
-                  totalAmount: double.parse(json['total_amount']),
-                  paidAmount: double.parse(json['paid_amount']),
-                  balance: double.parse(json['balance']),
-                  discount: double.parse(json['discount']),
-                  creditPeriodEndDate:
-                      DateTime.parse(json['credit_period_end_date']),
-                  paymentOption: json['payment_option'],
-                  createdAt: DateTime.parse(json['createdAt']),
-                  updatedAt: DateTime.parse(json['updatedAt']),
-                ))
-            .toList();
+        AppLogger.logInfo('Data received: $data');
+        return data.map((json) => Invoice.fromJson(json)).toList();
       } else {
-        AppLogger.logError(
-            'Failed to load invoices: ${response.statusCode}'); // Add print statement here
+        AppLogger.logError('Failed to load invoices: ${response.statusCode}');
         throw Exception('Failed to load invoices');
       }
     } catch (e) {
-      AppLogger.logError(
-          'Error fetching invoices: $e'); // Add print statement here
+      AppLogger.logError('Error fetching invoices: $e');
       throw Exception('Error: $e');
     }
   }
 
-  //get client by id
+  // get client by id
   static Future<Client> getClientById(int clientId) async {
-    final response = await http.get(Uri.parse('$baseUrl/client/$clientId'));
-    if (response.statusCode == 200) {
-      final Map<String, dynamic> data = json.decode(response.body);
-      return Client.fromJson(data);
-    } else {
-      throw Exception('Failed to get client details');
+    try {
+      print('Fetching client details for client ID: $clientId');
+      final response = await http.get(Uri.parse('$baseUrl/client/$clientId'));
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> data = json.decode(response.body);
+        print('Client data received for client ID $clientId: $data');
+        return Client.fromJson(data);
+      } else {
+        throw Exception('Failed to get client details');
+      }
+    } catch (e) {
+      print('Error fetching client details for client ID: $clientId: $e');
+      throw Exception('Error: $e');
     }
   }
-  
+
   //get client balance
   static Future<double> getClientBalance(int clientId) async {
     try {
@@ -63,8 +51,9 @@ class InvoiceService {
       if (response.statusCode == 200) {
         final jsonResponse = json.decode(response.body);
         // Search for the invoice corresponding to the selected client
-        final invoice = jsonResponse['invoices']
-            .firstWhere((invoice) => invoice['client_id'] == clientId, orElse: () => null);
+        final invoice = jsonResponse['invoices'].firstWhere(
+            (invoice) => invoice['client_id'] == clientId,
+            orElse: () => null);
         if (invoice != null) {
           // If the client has an invoice, return its balance
           final balance = double.tryParse(invoice['balance'] ?? '0') ?? 0;
@@ -84,5 +73,4 @@ class InvoiceService {
       throw Exception('Error: $e');
     }
   }
-
 }
